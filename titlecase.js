c2bd2d12887eaa4926c389bbc1d38f61b308fe55
EasonWan <easonflp@gmail.com>
2017-05-21 16:51:48 +0800

    learning to capitalize all first word in a sentence

---
+++ titlecase.js
@@ -0,0 +1,7 @@
+let titleCase = function(str){
+   
+  return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+
+}
+
+console.log(titleCase("I'm a little tea pot"));
