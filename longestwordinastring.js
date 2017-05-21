6d39c68512792ff175d57b985f95b6ce97355e79
EasonWan <easonflp@gmail.com>
2017-05-21 14:12:20 +0800

    learn to find longest word in a string

---
+++ longestwordinastring.js
@@ -0,0 +1,15 @@
+let findLongestWord = function(str) {
+
+  let words = str.split(" ");
+  var longestWord = 0;
+
+  for (var i=0; i<words.length; i++){
+    if (words[i].length > longestWord){
+      longestWord = words[i].length;
+    }
+  }
+  return longestWord;
+}
+
+console.log(findLongestWord("The quick brown fox jumped over the lazy dog"));
+
