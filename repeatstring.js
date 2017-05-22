ded3161df664947e0e12087c867220673fd4ff76
EasonWan <easonflp@gmail.com>
2017-05-22 15:30:53 +0800

    learn repeat string N times

---
+++ repeatstring.js
@@ -0,0 +1,14 @@
+let repeatStringNumTimes = function(str, num){
+
+  let accumulatedStr = '';
+
+  while (num > 0) {
+    accumulatedStr += str;
+    num--;
+  }
+
+  return accumulatedStr;
+}
+
+
+console.log(repeatStringNumTimes("abc", 3));
